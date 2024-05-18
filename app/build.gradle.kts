700cb0d30e020ed9e2ed8e49d07c204e58c962cf
Sandika <sandikasathsara806@gmail.com>
2024-05-18 21:47:05 +0530

    RailSynC_0.7 GPS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    id ("com.google.gms.google-services")
+    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -40,10 +42,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 
     // Map
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
--- app/src/main/java/uk/ac/wlv/railsyncsrilanka/Live_Location.java
+++ app/src/main/java/uk/ac/wlv/railsyncsrilanka/Live_Location.java
@@ -2,7 +2,9 @@ package uk.ac.wlv.railsyncsrilanka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Live_Location extends AppCompatActivity {
 
@@ -10,6 +12,13 @@ public class Live_Location extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_location);
+
+        findViewById(R.id.ML).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Live_Location.this, Live_Location_LN.class));
+            }
+        });
 
 
     }
